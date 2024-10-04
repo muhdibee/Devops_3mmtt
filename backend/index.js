47d2b29a93aca977266a290f63c45810db97a49f
IamLam163 <olamidealiyu@gmail.com>
2024-10-04 07:39:13 +0100

    local api

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ const Devops = [
   { classId: 1, name: "Celestine Ugwu", gender: "male" },
   { classId: 2, name: "Abel", gender: "male" },
   { classId: 3, name: "Fred Kanwai", gender: "male" },
+  { classId: 4, name: "Abdulqoyum Adeola Ilori", gender: "male" },
 ];
 
 app.get("/devops", async (req, res) => {
